944e04ddd4cb67a079f9c4d90e2df4701b5cbb02
Federico Gonzalez <federico.gonzalez@conduit.financial>
2024-04-18 16:09:28 -0300

    #17 Add solar_certificates endpoint

--- app/core/solar_emissions.py
+++ app/core/solar_emissions.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 import numpy as np
 import pandas as pd
 from core.solar import Solar
-from db.utils import get_co2_emissions_per_kwh
+from db.utils import get_client_settings, get_co2_emissions_per_kwh
 from db.db import session
 
 
@@ -13,14 +13,26 @@ def _fill_missing_data(df: pd.DataFrame, datetime_start: datetime, datetime_end:
         df.fillna(0, inplace=True)
         return df
 
-def calculate_co2_avoided(cli_id: int, loc_id:int, datetime_start: datetime, datetime_end: datetime, freq: str) -> Tuple[int, int, List[int], datetime, datetime, int]:
+def calculate_co2_avoided(cli_id: int, loc_id:int, datetime_start: datetime, datetime_end: datetime, freq: str) -> pd.DataFrame:
     solar = Solar(cli_id, loc_id, None, None, datetime_start, datetime_end, freq)
 
     solar.fetch_aggregated_by_loc_and_period()
     co2 = get_co2_emissions_per_kwh(session, solar.loc_id, datetime_start, datetime_end)    
+    client_settings = get_client_settings(session, cli_id)
+
+    cert_sold_pct = client_settings.loc['certSoldPorcentage']['cli_set_value'] or 0
+    cert_sold_pct = int(cert_sold_pct) / 100
+    cert_price = client_settings.loc['certPrice']['cli_set_value'] or 0
+    cert_price = int(cert_price)
+    
     co2 = _fill_missing_data(co2, datetime_start, datetime_end)
     co2 = co2.groupby(pd.Grouper(freq=freq)).sum().fillna(0)
-    df = solar.data_aggregated_by_loc_and_period.merge(co2, on='data_date', how='left')
-    df['co2_avoided'] = df['power'] * df['co2_per_kwh']
 
-    return df[['co2_avoided', 'from', 'to']]
+    df = solar.data_aggregated_by_loc_and_period.merge(co2, on='data_date', how='left')
+    df['cert_generated'] = df['power']
+    df['co2_avoided'] = df['power'] * df['co2_per_kwh']
+    df['cert_generated'] = df['power']
+    df['cert_sold'] = df['cert_generated'] * cert_sold_pct
+    df['price'] = df['cert_generated'] * cert_price
+    df['income'] = df['cert_sold'] * cert_price
+    return df[['co2_avoided', 'cert_sold', 'cert_generated', 'price', 'income', 'from', 'to']]
